999c5a138d6d3db30b21a18f450f7090db0e8171
Mitch <artfixmitch@gmail.com>
2018-11-17 02:03:56 +0300

    A Tiny JS World - first step

--- index.js
+++ index.js
@@ -7,8 +7,55 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const dog = {
+  species: 'dog',
+  name: 'Toby',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'woof-woof'
+};
+const cat = {
+  species: 'cat',
+  name: 'Milka',
+  gender: 'female',
+  legs: 4,
+  hands: 0,
+  saying: 'meow-meow'
+};
+const woman = {
+  species: 'human',
+  name: 'Emma',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  saying: 'Hello everyone!'
+};
+const man = {
+  species: 'human',
+  name: 'Alex',
+  gender: 'male',
+  legs: 2,
+  hands: 2,
+  saying: 'Hello everyone!'
+};
+const catWoman = {
+  species: 'catWoman',
+  name: 'Mia',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  saying: cat.saying
+};
 
-
+function say(species, name, gender, legs, hands, saying){
+   print(`species: ${species}; name: ${name}; gender: ${gender}; legs: ${legs}; hands: ${hands}; saying: ${saying}`, 'div');
+}
+say(dog.species, dog.name, dog.gender, dog.legs, dog.hands, dog.saying);
+say(cat.species, cat.name, cat.gender, cat.legs, cat.hands, cat.saying);
+say(woman.species, woman.name, woman.gender, woman.legs, woman.hands, woman.saying);
+say(man.species, man.name, man.gender, man.legs, man.hands, man.saying);
+say(catWoman.species, catWoman.name, catWoman.gender, catWoman.legs, catWoman.hands, catWoman.saying);
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -17,6 +64,7 @@
    However, please, REFRAIN from improving visuals at least until your code is reviewed
    so code reviewers might focus on a single file that is index.js.
    */
+
 
 /* Print examples:
    print('ABC');
@@ -27,5 +75,5 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
+ 
 
